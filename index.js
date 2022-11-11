2a6c25ed19fec3f2ef228a98c78c2900665e87ad
Uvais Ahmad <uvss21@gmail.com>
2022-11-11 16:16:42 +0530

    deploy setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const db = require('./config/mongoose');
+const dotenv = require('dotenv').config();
 
 app.use(express.urlencoded());
 
